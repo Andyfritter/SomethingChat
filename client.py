e4f19c3cef09cbb6ab08280c3a20d20148ec88ff
Andyfritter <andyfritter@gmail.com>
2012-10-20 16:50:48 -0400

    ADDED SOME SHIT

--- client.py
+++ client.py
@@ -4,12 +4,16 @@ from config import *
 class Client():
 	def __init__(self):
                 self.name = raw_input('Enter Name: ')
-		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.s.connect((HOST, PORT))
-		data = s.recv(1024)
-		self.s.close()
+                try: self.host = HOST
+                except: self.host = raw_input('Enter Host\'s IP: ')
+                self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.s.connect((self.host, PORT))
+                data = s.recv(1024)
+                self.s.close()
 		print repr(data)
 
 	def say(self, msg):
                 self.s.sendall(self.name + ': ' + msg)
                 
+
+        
